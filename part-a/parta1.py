2475c21420980e8495987c568d414c7639b2ed12
fwadegithub <fwade@student.unimelb.edu.au>
2021-04-15 09:23:40 +0000

    final copy of parta1

--- part-a/parta1.py
+++ part-a/parta1.py
@@ -2,20 +2,26 @@ import pandas as pd
 import argparse
 import numpy as np
 
+### sub task 1
+
 # import the full covid dataframe and create the dataframe for the relevant covid data
 covid_data = pd.read_csv('owid-covid-data.csv', encoding = 'ISO-8859-1')
 
 relevant_data = covid_data[['location','date','new_cases','new_deaths']]
 
 # convert the date string to an integer representing the month it is in.
+dates = []
+bad_index = []
 for i in relevant_data.index:
     if relevant_data.date[i][-4:] != '2020':
-        relevant_data = relevant_data.drop(i)
+        bad_index.append(i)
         continue
-    month_index = relevant_data.date[i][-7:-5]
-    relevant_data.date[i] = int(month_index)
-    
+    month_index = int(relevant_data.date[i][-7:-5])
+    dates.append(month_index)
+
+relevant_data = relevant_data.drop(bad_index)
 relevant_data.index = list(range(len(relevant_data)))
+relevant_data['date'] = dates
 
 # create a dictionary of the indexes corresponding to each location
 location_dic = {}
@@ -58,8 +64,8 @@ monthly_covid_data = pd.DataFrame(full_data, columns = ['location', 'month','new
 
 # make new location dictionary of indexes relevant to each location in the new Data Frame
 location_dic2 = {}
-for i in range(len(new_data.index)):
-    if new_data.location[i] not in location_dic2:
+for i in range(len(monthly_covid_data.index)):
+    if monthly_covid_data.location[i] not in location_dic2:
         location_dic2[monthly_covid_data.location[i]] = [i]
     else:
         location_dic2[monthly_covid_data.location[i]].append(i)
@@ -79,4 +85,21 @@ for location in location_dic2.keys():
 monthly_covid_data['total_cases'] = total_cases
 monthly_covid_data['total_deaths'] = total_deaths
 
-#covid_data
+### sub task 2
+
+# find the case fatality rate and add it to the data frame
+fatality_rates = []
+for index in monthly_covid_data.index:
+    fatality_rate = monthly_covid_data.new_deaths[index]/monthly_covid_data.new_cases[index]
+    fatality_rates.append(fatality_rate)
+    
+monthly_covid_data['case_fatality_rate'] = fatality_rates
+
+# put the columns in the correct order
+# rearranging the columns
+column_names = ['location','month','case_fatality_rate','total_cases','total_cases','new_cases','total_deaths','new_deaths']
+monthly_covid_data = monthly_covid_data.reindex(columns = column_names)
+
+# export as csv and print the first 5 rows
+print(monthly_covid_data.head())
+monthly_covid_data.to_csv(r'owid-covid-data-2020-monthly.cvs')
