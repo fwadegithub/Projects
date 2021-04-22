01836b01e5b60468d6362267c64ff02828c7cf1c
fwadegithub <fwade@student.unimelb.edu.au>
2021-04-22 07:02:50 +0000

    partb2

--- part-b/partb2.py
+++ part-b/partb2.py
@@ -3,3 +3,17 @@ import re
 import os
 import sys
 
+# create a function to preprocess the article
+def preprocess(article):
+    os.chdir(r"cricket")
+    article = open(article, "r")
+    article_text = article.read()
+    preproc_text = re.sub(r'[^a-zA-Z]+', ' ', article_text)
+    return preproc_text.lower()
+
+# extract the article name from the input, precprocess it and print it to standard output
+article_name = sys.argv[1]
+article = str(article_name[-7:])
+
+preprocessed_article = preprocess(article)
+print(preprocessed_article, file = sys.stdout)
