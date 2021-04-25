e640587156698f0132c5543a2d66fe7a1dade868
fwadegithub <fwade@student.unimelb.edu.au>
2021-04-25 07:31:04 +0000

    25 april

--- partb2.py
+++ partb2.py
@@ -3,3 +3,21 @@ import re
 import os
 import sys
 
+# record the current directory to change back at after execution
+fulldir = os.getcwd()
+os.chdir(r"cricket")
+
+# create a function to preprocess the article
+def preprocess(article_name):
+    article_text = open(article_name, "r").read()
+    preproc_text = re.sub(r'[^a-zA-Z]+', ' ', article_text)
+    return preproc_text.lower()
+
+# extract the article name from the input, precprocess it and print it to standard output
+article_name = sys.argv[1]
+article = str(article_name[-7:])
+
+preprocessed_article = preprocess(article)
+print(preprocessed_article, file = sys.stdout)
+
+os.chdir(fulldir)
